a260c6566d3a73f01c2d72d81073e29a20649116
Alex Pickering <33042726+MechaFinch@users.noreply.github.com>
2019-02-12 13:43:45 -0500

    Initial commit

---
+++ src/game/TerminalHangman.java
@@ -0,0 +1,14 @@
+package game;
+
+/**
+ * Its that main class boiiiiiiiiiiiiiiiiiiiiiii
+ * <p> Hangman game! School projects! WOOOO
+ * <p> Specifically this file has the command interpreter and lets you do stuff
+ * 
+ * @author Alex Pickering
+ */
+public class TerminalHangman {
+	public static void main(String[] args) {
+		System.out.println("Hello, Placeholders!");
+	}
+}
